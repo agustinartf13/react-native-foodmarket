af50a4e902ca33dd7261e4dacb1f8a284d41b8da
agustinartf13 <agustina123379@gmail.com>
2021-02-28 14:22:21 +0800

    setup asyncstorage

--- src/components/molecules/HomeProfile/index.js
+++ src/components/molecules/HomeProfile/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
-import { ProfileDummy } from '../../../assets';
+import {ProfileDummy} from '../../../assets';
+import {getData} from '../../../utils';
 
 const HomeProfile = () => {
+  const [photo, setPhoto] = useState(ProfileDummy);
+  useEffect(() => {
+    getData('userProfile').then((res) => {
+      console.log('user profile: ', res);
+      setPhoto({uri: res.profile_photo_url});
+    });
+  }, []);
   return (
     <View style={styles.profileContainer}>
       <View>
         <Text style={styles.appName}>FoodMarket</Text>
         <Text style={styles.desc}>Let's get some foods</Text>
       </View>
-      <Image source={ProfileDummy} style={styles.profile} />
+      <Image source={photo} style={styles.profile} />
     </View>
   );
 };
--- src/redux/action/auth.js
+++ src/redux/action/auth.js
@@ -1,22 +1,32 @@
 import Axios from 'axios';
-import { showMessage } from '../../utils';
-import { setLoading } from './global';
+import {showMessage, storeData} from '../../utils';
+import {setLoading} from './global';
 
 const API_HOST = {
   url: 'http://foodmarket-backend.buildwithangga.id/api',
 };
 
-export const signUpAction = (dataRegister, photoReducer, navigation) => (dispatch) => {
+export const signUpAction = (dataRegister, photoReducer, navigation) => (
+  dispatch,
+) => {
   Axios.post(`${API_HOST.url}/register`, dataRegister)
     .then((res) => {
       console.log('data success: ', res.data);
+
+      const profile = res.data.data.user;
+      const token = `${res.data.data.token_type} ${res.data.data.access_token}`;
+     
+      // data user & token
+      storeData('userProfile', profile);
+      storeData('token', {value: token});
+
       if (photoReducer.isUploadPhoto) {
         const photoForUpload = new FormData();
         photoForUpload.append('file', photoReducer);
 
         Axios.post(`${API_HOST.url}/user/photo`, photoForUpload, {
           headers: {
-            Authorization: `${res.data.data.token_type} ${res.data.data.access_token}`,
+            Authorization: token,
             'Content-Type': 'multipart/form-data',
           },
         })
